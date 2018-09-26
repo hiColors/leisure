0d2a6a7cb7fca271bf964d7f059a7c2fec833ed9
李伟超 <liweichao0102@gmail.com>
2018-09-26 18:22:38 +0800

    修复 httpclient 阻塞问题

--- common-fegin/src/main/java/com/github/hicolors/leisure/common/feign/customiz/FeignConfiguration.java
+++ common-fegin/src/main/java/com/github/hicolors/leisure/common/feign/customiz/FeignConfiguration.java
@@ -3,8 +3,13 @@ package com.github.hicolors.leisure.common.feign.customiz;
 
 import com.github.hicolors.leisure.common.utils.JsonUtils;
 import feign.RequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 import feign.codec.ErrorDecoder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -21,7 +26,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import java.util.*;
 
 /**
  * Feign 配置
@@ -32,21 +37,29 @@ import java.util.Enumeration;
 @Configuration
 public class FeignConfiguration {
 
+    private static final String SPLIT_CHARACTER = ";";
+
+    @Value("feign.penetrate.headers:user-agent")
+    private String penetrateHeaders;
+
     @Bean
-    public ResponseEntityDecoder feignDecoder() {
+    @ConditionalOnMissingBean(Decoder.class)
+    public Decoder feignDecoder() {
         HttpMessageConverter<?> jacksonConverter = new MappingJackson2HttpMessageConverter(JsonUtils.getObjectMapper());
         ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
         return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
     }
 
     @Bean
-    public SpringEncoder feignEncoder() {
+    @ConditionalOnMissingBean(Encoder.class)
+    public Encoder feignEncoder() {
         HttpMessageConverter<?> jacksonConverter = new MappingJackson2HttpMessageConverter(JsonUtils.getObjectMapper());
         ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
         return new SpringEncoder(objectFactory);
     }
 
     @Bean
+    @ConditionalOnMissingBean(ErrorDecoder.class)
     public ErrorDecoder errorDecoder() {
         return new ColorsErrorDecoder();
     }
@@ -57,16 +70,24 @@ public class FeignConfiguration {
         return new WebMvcRegistrations() {
             @Override
             public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-                return new FeignRequestMappingHandlerMapping();
+                return new RequestMappingHandlerMapping() {
+                    @Override
+                    protected boolean isHandler(Class<?> beanType) {
+                        boolean flag = AnnotatedElementUtils.hasAnnotation(beanType, FeignClient.class);
+                        return super.isHandler(beanType) && !flag;
+                    }
+                };
             }
         };
     }
 
     @Bean
     public RequestInterceptor headerInterceptor() {
+        List<String> headerList = Arrays.asList(penetrateHeaders.split(SPLIT_CHARACTER));
+        Set<String> headers = new HashSet<>();
+        headerList.forEach(e -> headers.add(e.toLowerCase()));
         return requestTemplate -> {
-            ServletRequestAttributes attributes = (ServletRequestAttributes)
-                    RequestContextHolder.getRequestAttributes();
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
             assert attributes != null;
             HttpServletRequest request = attributes.getRequest();
             Enumeration<String> headerNames = request.getHeaderNames();
@@ -74,17 +95,15 @@ public class FeignConfiguration {
                 while (headerNames.hasMoreElements()) {
                     String name = headerNames.nextElement();
                     String values = request.getHeader(name);
-                    requestTemplate.header(name, values);
+                    for (String header : headers) {
+                        if (StringUtils.equalsIgnoreCase(header, name)) {
+                            requestTemplate.header(name, values);
+                            break;
+                        }
+                    }
                 }
             }
         };
     }
 
-    private static class FeignRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
-        @Override
-        protected boolean isHandler(Class<?> beanType) {
-            boolean flag = AnnotatedElementUtils.hasAnnotation(beanType, FeignClient.class);
-            return super.isHandler(beanType) && !flag;
-        }
-    }
 }
--- common-utils/src/main/java/com/github/hicolors/leisure/common/utils/DingTalkUtils.java
+++ common-utils/src/main/java/com/github/hicolors/leisure/common/utils/DingTalkUtils.java
@@ -24,6 +24,9 @@ import java.util.Map;
 public class DingTalkUtils {
 
     private static final String ENV = System.getProperty("env");
+
+    private static final HttpClient HTTP_CLIENT = HttpClients.createDefault();
+
     private static final String STANDARD_TEMPLATE =
             "<font face=\"微软雅黑\" color=#ff0000 size=4> {0} </font>  \r\n" +
                     "> ENV : {1}  \r\n" +
@@ -34,30 +37,45 @@ public class DingTalkUtils {
                     "> 额外信息 : {6}  \r\n" +
                     "> 时间 : {7}  \r\n" +
                     "> 异常信息 : {8}  \r\n";
-    private static HttpClient httpclient = HttpClients.createDefault();
+
 
     /**
      * @param webhook 钉钉机器人地址
+     * @param warning 标准预警信息
      */
     public static void send(String webhook, Warning warning) {
-        String message = buildStandardTemplate(warning);
-        try {
-            HttpPost httppost = new HttpPost(webhook);
-            httppost.addHeader("Content-Type", "application/json; charset=utf-8");
-            StringEntity se = new StringEntity(message, "utf-8");
-            httppost.setEntity(se);
-            HttpResponse response = httpclient.execute(httppost);
-            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                log.info("预警发送成功 -> " + message);
-            }
-        } catch (IOException e) {
-            log.warn("预警发送失败 -> " + message);
-        }
+        sendMarkdown(webhook, buildStandardTemplate(warning));
     }
 
 
+    public static void sendText(String webhook, String text) {
+        send(webhook, MsgType.TEXT, text);
+    }
+
+    public static void sendMarkdown(String webhook, String text) {
+        send(webhook, MsgType.MARKDOWN, text);
+    }
+
+    public static synchronized void send(String webhook, MsgType msgType, String text) {
+        String body = getDingTalkPostBody(msgType, text);
+        HttpPost httppost = new HttpPost(webhook);
+        try {
+            httppost.addHeader("Content-Type", "application/json; charset=utf-8");
+            StringEntity se = new StringEntity(body, "utf-8");
+            httppost.setEntity(se);
+            HttpResponse response = HTTP_CLIENT.execute(httppost);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                log.info("预警发送成功 -> " + body);
+            }
+        } catch (IOException e) {
+            log.warn("预警发送失败 -> " + body);
+        } finally {
+            httppost.reset();
+        }
+    }
+
     private static String buildStandardTemplate(Warning warning) {
-        String message = MessageFormat.format(STANDARD_TEMPLATE,
+        return MessageFormat.format(STANDARD_TEMPLATE,
                 warning.getTitle(),
                 ENV,
                 warning.getTraceId(),
@@ -67,17 +85,48 @@ public class DingTalkUtils {
                 JsonUtils.serialize(warning.getExtraInfo()),
                 DateFormatUtils.format(warning.getDate(), "yyyy-MM-dd HH:mm:ss.SSS"),
                 warning.getExceptionMsg());
-        return getMarkdownMsg(message);
     }
 
-
-    private static String getMarkdownMsg(String text) {
+    public static String getDingTalkPostBody(MsgType msgType, String msg) {
         Map<String, Object> result = new HashMap<>(2);
-        result.put("msgtype", "markdown");
-        Map<String, Object> markdown = new HashMap<>(2);
-        markdown.put("title", "钉钉机器人预警");
-        markdown.put("text", text);
-        result.put("markdown", markdown);
+        result.put("msgtype", msgType.getType());
+        switch (msgType) {
+            case MARKDOWN:
+                Map<String, Object> markdown = new HashMap<>(2);
+                markdown.put("title", "钉钉机器人预警");
+                markdown.put("text", msg);
+                result.put("markdown", markdown);
+                break;
+            case TEXT:
+                Map<String, Object> text = new HashMap<>(2);
+                text.put("title", "钉钉机器人预警");
+                text.put("content", msg);
+                result.put("text", text);
+                break;
+            default:
+                break;
+        }
         return JsonUtils.serialize(result);
+    }
+
+    public enum MsgType {
+        /**
+         * 钉钉发送消息text类型
+         */
+        TEXT("text"),
+        /**
+         * 钉钉发送消息markdown类型
+         */
+        MARKDOWN("markdown");
+
+        private final String type;
+
+        MsgType(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
     }
 }
