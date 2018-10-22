848643dabd82b4bc04a3911e96de5f8d262f916b
李伟超 <liweichao0102@gmail.com>
2018-10-22 13:43:39 +0800

    add ColorsBeanUtils
    copyPropertiesNonNull

---
+++ common-utils/src/test/java/com/github/hicolors/leisure/common/utils/A.java
@@ -0,0 +1,39 @@
+package com.github.hicolors.leisure.common.utils;
+
+import lombok.Data;
+
+public class A {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public A setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public A setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public A setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+}
---
+++ common-utils/src/test/java/com/github/hicolors/leisure/common/utils/AModel.java
@@ -0,0 +1,38 @@
+package com.github.hicolors.leisure.common.utils;
+
+import lombok.Data;
+
+public class AModel {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public AModel setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public AModel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public AModel setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+}
