e9964eb2d9c7518a672efe0d6ce1b05c4205c468
李伟超 <liweichao0102@gmail.com>
2019-01-25 13:52:18 +0800

    增加 metrics 模块，结合 Spring Boot Actuator 对服务部分指标进行监控。

---
+++ common-metrics/src/main/java/com/github/life/lab/leisure/common/metrics/configuration/CommonMetricsAutoConfigration.java
@@ -0,0 +1,25 @@
+package com.github.life.lab.leisure.common.metrics.configuration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * CommonMetricsAutoConfigration
+ *
+ * @author weichao.li (liweichao0102@gmail.com)
+ * @date 2018/9/11
+ */
+@Configuration
+public class CommonMetricsAutoConfigration {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+}
